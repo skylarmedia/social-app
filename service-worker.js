cb9503c675b48b14dc9d73fe02c413f7624356e5
Edward <you@email.com>
2019-10-22 01:33:42 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/social-app/precache-manifest.e907ecb8f2e6716498fd764d9dcc8dab.js"
+  "/social-app/precache-manifest.513864a06c4434883714f40e936f180d.js"
 );
 
 self.addEventListener('message', (event) => {
